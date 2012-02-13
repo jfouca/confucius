8d19f7100e5dce31035135b11fb3b292c9123d17
Nicolas Bazire <nicolas.bazire@gmail.com>
2012-02-13 17:46:10 +0100

    new proxy model for Users to enforce custom constraints

--- confucius/auth/models.py
+++ confucius/auth/models.py
@@ -2,6 +2,23 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
+class ConfuciusUser(User):
+    class Meta:
+        proxy = True
+
+    def clean(self):
+        from django.core import exceptions, validators
+        for field in [self.last_name, self.email]:
+            if(field in validators.EMPTY_VALUES):
+                raise exceptions.ValidationError(
+                    models.Field.default_error_messages['blank'])
+        super(ConfuciusUser, self).clean()
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super(ConfuciusUser, self).save(*args, **kwargs)
+
+
 class Language(models.Model):
     code = models.CharField(max_length=2)
     name = models.CharField(max_length=40)
@@ -11,7 +28,7 @@ class Language(models.Model):
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User)
+    user = models.OneToOneField(ConfuciusUser)
     secondary_email = models.EmailField(blank=True)
     primary_postal_address = models.TextField()
     secondary_postal_address = models.TextField(blank=True)
