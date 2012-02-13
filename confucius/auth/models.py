de5c393d3dc183c2bd4f35e252457a8cde9fe9af
Nicolas Bazire <nicolas.bazire@gmail.com>
2012-02-13 15:37:06 +0100

    flake8 compliance

--- confucius/auth/models.py
+++ confucius/auth/models.py
@@ -1,16 +1,18 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
+
 
 class Language(models.Model):
     code = models.CharField(max_length=2)
     name = models.CharField(max_length=40)
-        
+
     def __unicode__(self):
         return self.name
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User)
     secondary_email = models.EmailField(blank=True)
     primary_postal_address = models.TextField()
-    secondary_postal_address = models.TextField(blank=True) 
+    secondary_postal_address = models.TextField(blank=True)
     languages = models.ManyToManyField(Language)
