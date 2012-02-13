f6d9048d479e06445faf94d110c433ce5558f928
Lucas Fernandes <lucskywalkerzero@gmail.com>
2012-02-13 15:26:11 +0100

    Added language entity

--- confucius/auth/models.py
+++ confucius/auth/models.py
@@ -1,17 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Profile(models.Model):
+class Language(models.Model):
+    code = models.CharField(max_length=2)
+    name = models.CharField(max_length=40)
+        
+    def __unicode__(self):
+        return self.name
 
+class Profile(models.Model):
     user = models.OneToOneField(User)
     secondary_email = models.EmailField(blank=True)
     primary_postal_address = models.TextField()
     secondary_postal_address = models.TextField(blank=True) 
-    languages = models.ManyToManyField("Language")
-
-class Language(models.Model):
-    code = models.CharField(max_length=2)
-    value = models.CharField(max_length=40)
-
-    
-
+    languages = models.ManyToManyField(Language)
