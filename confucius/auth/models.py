72d8f8ab5b6f54e740c886f83367443e33517206
Lucas Fernandes <lucskywalkerzero@gmail.com>
2012-02-13 14:34:04 +0100

    models v1.1 - Ajout des blank

--- confucius/auth/models.py
+++ confucius/auth/models.py
@@ -4,9 +4,9 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
 
     user = models.OneToOneField(User)
-    secondary_email = models.EmailField()
+    secondary_email = models.EmailField(blank=True)
     primary_postal_address = models.TextField()
-    secondary_postal_address = models.TextField() 
+    secondary_postal_address = models.TextField(blank=True) 
     languages = models.ManyToManyField("Language")
 
 class Language(models.Model):
