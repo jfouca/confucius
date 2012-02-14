1f809cba925d9e7e2e33d55fedff0e8f1de37fef
Nicolas Bazire <nicolas.bazire@gmail.com>
2012-02-14 13:39:49 +0100

    application urls

--- confucius/views/authviews.py
+++ confucius/views/authviews.py
@@ -1,4 +1,9 @@
-from django.contrib.auth.views import login,logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import login, logout
 
-def profile(request, user_name):
-    pass
+from django.http import HttpResponse
+
+
+@login_required
+def profile(request):
+    return HttpResponse(request.user.username)
