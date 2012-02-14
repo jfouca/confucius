7adc4c641873fd6eb00064e338acac98d1aff527
Yann Lemaulf <ylem39@gmail.com>
2012-02-14 11:37:55 +0100

    add login test

--- confucius/tests/authviews.py
+++ confucius/tests/authviews.py
@@ -1,19 +1,39 @@
 from django.utils import unittest
 from django.test.client import Client
-
 from confucius.models import User
 
 
-class LoginTestCase(unittest.TestCase):
+class LoginTestCase(unittest.TestCase): 
     def setUp(self):
+    	#Creating a mock webClient
         self.client = Client()
+        #Deleting all user in database before creating our test_user blu
+        User.objects.all().delete()
+        u = User.objects.create(username="blu", password="blu", last_name="blu",email="blu@blu.fr")
+    	u.set_password("blu")
+     	u.save()
 
-    def test_login(self):
-        u = User.objects.create(
-                username="blu", password="blu", last_name="blu",
-                email="blu@blu.fr")
-        u.set_password("blu")
-        u.save()
+    def test_succesfull_login(self):
         response = self.client.get('/login/')
         self.assertEquals(200, response.status_code)
         self.assertTrue(self.client.login(username="blu", password="blu"))
+        
+    def test_empty_username_login(self):
+        response = self.client.get('/login/')
+        self.assertEquals(200, response.status_code)
+        self.assertFalse(self.client.login(username="", password="blu"))
+        
+    def test_empty_password_login(self):
+        response = self.client.get('/login/')
+        self.assertEquals(200, response.status_code)
+        self.assertFalse(self.client.login(username="blu", password=""))
+        
+    def test_wrong_username_login(self):
+        response = self.client.get('/login/')
+        self.assertEquals(200, response.status_code)
+        self.assertFalse(self.client.login(username="bla", password="blu"))
+        
+    def test_wrong_password_login(self):
+        response = self.client.get('/login/')
+        self.assertEquals(200, response.status_code)
+        self.assertFalse(self.client.login(username="blu", password="bla"))
