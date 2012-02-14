ebc6c78b3ef20b2215063b460b5d7fbd057d12ca
Yann Lemaulf <ylem39@gmail.com>
2012-02-14 14:34:12 +0100

    add change password function and test

--- confucius/tests/authviews.py
+++ confucius/tests/authviews.py
@@ -37,3 +37,20 @@ class LoginTestCase(unittest.TestCase):
         response = self.client.get('/login/')
         self.assertEquals(200, response.status_code)
         self.assertFalse(self.client.login(username="blu", password="bla"))
+
+class PasswordTestCase(unittest.TestCase):
+    def setUp(self):
+        #Creating a mock webClient
+	self.client = Client()
+        #Deleting all user in database before creating our test_user blu
+        User.objects.all().delete()
+        u = User.objects.create(username="blu", password="blu", last_name="blu",email="blu@blu.fr")
+    	u.set_password("blu")
+     	u.save()
+
+    def test_change_password(self):
+	response = self.client.get('/login/')
+	self.assertEquals(200, response.status_code)
+	self.client.login(username="blu", password="blu")
+	self.client.post('/change-password/',{'old_password':'blu','new_password1':'bla','new_password2':'bla'})
+     	self.assertTrue(u.check_password('bla'))
--- confucius/views/authviews.py
+++ confucius/views/authviews.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.views import login,logout
+from django.contrib.auth.views import login,logout,password_change
 
 def profile(request, user_name):
     pass
