8322fc07e4475cd4b8e6f9af505b47e133eab335
Nicolas Bazire <nicolas.bazire@gmail.com>
2012-02-16 10:28:19 +0100

    re added old tests, heavy rewriting needed

--- confucius/tests/__init__.py
+++ confucius/tests/__init__.py
@@ -1,1 +1,2 @@
 from confucius.tests.authmodels import (AccountTestCase)
+from confucius.tests.authviews import (LoginTestCase, LogoutTestCase, PasswordChangeTestCase)
--- confucius/tests/authviews.py
+++ confucius/tests/authviews.py
@@ -1,24 +1,32 @@
 from django.utils import unittest
 from django.test.client import Client
 
-from confucius.models import User
+from confucius.models import Account
 
+
+# NEED SOME HEAVY REWRITING
 
 class LoginTestCase(unittest.TestCase):
+    sample_info = {
+        'email': 'alice@bob.com',
+        'last_name': 'Blu',
+        'password': 'red',
+    }
+    sample_email = 'bob@alice.com'
+
     def setUp(self):
+        Account.objects.all().delete()
+        self.account = Account.objects.create(**self.sample_info)
         self.client = Client()
-        User.objects.all().delete()
-        u = User.objects.create(
-                username="blu", password="blu", last_name="blu",
-                email="blu@blu.fr")
-        u.set_password("blu")
-        u.save()
 
     def test_succesfull_login(self):
-        response = self.client.get('/login/')
-        self.assertEquals(200, response.status_code)
-        self.assertTrue(self.client.login(username="blu", password="blu"))
+        response = self.client.post('/login/',
+            {'username': self.sample_info['email'],
+            'password': self.sample_info['password']})
 
+        self.assertEquals(200, response.status_code)
+
+"""
     def test_empty_username_login(self):
         response = self.client.get('/login/')
         self.assertEquals(200, response.status_code)
@@ -38,3 +46,41 @@ class LoginTestCase(unittest.TestCase):
         response = self.client.get('/login/')
         self.assertEquals(200, response.status_code)
         self.assertFalse(self.client.login(username="blu", password="bla"))
+"""
+
+
+class ChangePasswordTestCase(unittest.TestCase):
+    sample_info = {
+        'email': 'alice@bob.com',
+        'last_name': 'Blu',
+        'password': 'red',
+    }
+    sample_email = 'bob@alice.com'
+
+    def setUp(self):
+        Account.objects.all().delete()
+        self.account = Account.objects.create(**self.sample_info)
+        self.client = Client()
+
+    def test_change_password(self):
+        response = self.client.get('/login/')
+        self.assertEquals(200, response.status_code)
+        #self.client.post('/change-password/',{'old_password':'blu','new_password1':'bla','new_password2':'bla'})
+
+
+class LogoutTestCase(unittest.TestCase):
+    sample_info = {
+        'email': 'alice@bob.com',
+        'last_name': 'Blu',
+        'password': 'red',
+    }
+    sample_email = 'bob@alice.com'
+
+    def setUp(self):
+        Account.objects.all().delete()
+        self.account = Account.objects.create(**self.sample_info)
+        self.client = Client()
+    
+    def test_successfull_logout(self):
+        response = self.client.get('/login/')
+        self.assertEquals(200, response.status_code)
