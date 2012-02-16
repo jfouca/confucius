f5c402e1123f591b067cab33f34d24a1cc447762
Nicolas Bazire <nicolas.bazire@gmail.com>
2012-02-16 16:03:04 +0100

    updated account form and views

--- confucius/views/authviews.py
+++ confucius/views/authviews.py
@@ -1,24 +1,46 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.forms.models import modelform_factory, inlineformset_factory
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
-from confucius.models import Account
-from confucius.forms import AccountForm
+from confucius.models import Account, Language, EmailAddress, PostalAddress
 
 
 @login_required
 def main_page(request):
     return render_to_response(
-            'index.html',  context_instance=RequestContext(request))
+        'index.html',  context_instance=RequestContext(request))
 
 
 @login_required
 def edit_account(request):
     account = Account.objects.get(user=request.user)
-    form = AccountForm(instance=account)
-    return render_to_response('account/edit_account.html', {'form': form, 'account': account},
-            context_instance=RequestContext(request))
+    UserForm = modelform_factory(User, fields=('first_name', 'last_name',))
+    AccountForm = modelform_factory(Account, exclude=('user',))
+    EmailAddressFormSet = inlineformset_factory(Account, EmailAddress)
+    PostalAddressFormSet = inlineformset_factory(Account, PostalAddress)
+
+    if request.POST:
+        user_form = UserForm(request.POST, instance=account.user)
+        account_form = AccountForm(request.POST, instance=account)
+        emailaddress_set = EmailAddressFormSet(request.POST, instance=account)
+        postaladdress_set = PostalAddressFormSet(request.POST, instance=account)
+
+        for f in (user_form, account_form, emailaddress_set, postaladdress_set):
+            if f.is_valid():
+                f.save()
+    else:
+        user_form = UserForm(instance=account.user)
+        account_form = AccountForm(instance=account)
+        emailaddress_set = EmailAddressFormSet(instance=account)
+        postaladdress_set = PostalAddressFormSet(instance=account)
+
+    return render_to_response('account/edit_account.html', {
+        'account': user_form, 'emailaddress': emailaddress_set, 'postaladdress': postaladdress_set,
+        'form': account_form},
+        context_instance=RequestContext(request))
 
 
 def language_autocomplete(request):
