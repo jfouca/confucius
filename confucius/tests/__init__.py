a54157b73de3437a8bf7332c8898de8cc4d31bd7
Yann Lemaulf <ylem39@gmail.com>
2012-02-16 12:31:14 +0100

    Add conference models and tests

--- confucius/tests/__init__.py
+++ confucius/tests/__init__.py
@@ -1,2 +1,3 @@
 from confucius.tests.authmodels import (AccountTestCase)
 from confucius.tests.authviews import (LoginTestCase, LogoutTestCase, PasswordChangeTestCase)
+from confucius.tests.confmodels import (ConfTestCase, AlertTestCase, MessageTemplateTestCase, RoleTestCase, ConferenceAccountRoleTestCase)
