bfcff36da0a87c5fb9c92b6d204beaffd8b8d00b
Nicolas Bazire <nicolas.bazire@gmail.com>
2012-02-16 02:43:36 +0100

    account backend

---
+++ confucius/backends.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.models import User
+
+from confucius.models import Account
+
+
+class AccountBackend(object):
+    supports_object_permissions = False
+    supports_anonymous_user = False
+    supports_inactive_user = False
+
+    def authenticate(self, username=None, password=None):
+        try:
+            account = Account.objects.get(emailaddress__value__exact=username)
+            if account.check_password(password):
+                return account.user
+        except Account.DoesNotExist:
+            pass
+        return None
+
+    def get_user(self, user_id):
+        try:
+            return User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return None
