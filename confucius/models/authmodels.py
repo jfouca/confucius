17f663a6e6854f51104bfa7354361ea2bb1f46bb
Jeremy FOUCAULT <foucault.jeremy@gmail.com>
2012-02-16 16:16:47 +0100

    added feature main to addresses. Still to do

--- confucius/models/authmodels.py
+++ confucius/models/authmodels.py
@@ -3,8 +3,9 @@ from datetime import datetime
 from django.conf import settings
 from django.contrib.auth.models import User
 from django.db import models
-
+from django.db.models.signals import pre_delete
 from confucius.utils import email_to_username
+from django.db import IntegrityError
 
 
 class AccountQuerySet(models.query.QuerySet):
@@ -54,7 +55,20 @@ class Account(models.Model):
                 'check_password', 'set_password'):
             return getattr(self.user, name)
         return super(Account, self).__getattr__(name)
-        
+       
+#
+#
+#
+#
+#   ATTENTION !!!!  il semble que la methode setattr cause un soucis dans la page d'admin django, quand on clique sur : "save and add an other"
+#
+#
+#
+#
+#   
+
+
+ 
     def __setattr__(self, name, value):
         if name in ('username', 'first_name', 'last_name', 'is_active',
                 'check_password', 'set_password'):
@@ -97,15 +111,15 @@ class Address(models.Model):
     def __unicode__(self):
         return self.value
 
+# TO DO  --> Check if there is one Main Address and one only ! 
 
 class EmailAddress(Address):
-    value = models.EmailField(unique=True)
-
+    value = models.EmailField(unique=True, verbose_name="Email")
+    
 
 class PostalAddress(Address):
-    name = models.CharField(max_length=31)
-    value = models.TextField()
-
+    value = models.TextField(verbose_name="Address")
+    
 
 class Language(models.Model):
     code = models.CharField(max_length=5)
@@ -116,3 +130,15 @@ class Language(models.Model):
 
     def __unicode__(self):
         return self.value
+        
+def delete_emailaddress(sender, instance, **kwargs):
+    if instance.main == True :
+        raise Exception, "This email address is tagged as Main, you should not delete it"
+
+pre_delete.connect(delete_emailaddress, sender=EmailAddress)
+
+def delete_postaladdress(sender, instance, **kwargs):
+    if instance.main == True :
+        raise Exception, "This postal address is tagged as Main, you should not delete it"
+
+pre_delete.connect(delete_postaladdress, sender=PostalAddress)
