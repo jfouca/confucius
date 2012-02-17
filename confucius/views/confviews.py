0796a1752dcf223c47e3f3ce6c817558c4fb4428
raphael <raphoun@gmail.com>
2012-02-17 16:34:55 +0100

    view for edit conference and draf template to test it

--- confucius/views/confviews.py
+++ confucius/views/confviews.py
@@ -4,10 +4,37 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 from confucius.models import Conference, ConferenceAccountRole
-
+from django.forms.models import modelform_factory
+ 
+ 
 @login_required
 def list_conference(request) :
         
         conferences_president = Conference.objects.filter(president=request.user)
         conferences_with_role = ConferenceAccountRole.objects.filter(account=request.user)
-        return render_to_response('conference/list_conference.html', { 'conferences_president' : conferences_president ,'conferences_with_role' : conferences_with_role }, context_instance=RequestContext(request))
+        return render_to_response('conference/list_conference.html',
+        { 'conferences_president' : conferences_president ,'conferences_with_role' : conferences_with_role },
+        context_instance=RequestContext(request))
+
+        
+@login_required
+def edit_conference(request, conf_id) :
+	ConferenceForm = modelform_factory(Conference, exclude=('accounts','title','president'))
+	conference =  Conference.objects.get(pk=conf_id)
+        if request.POST:
+
+	  form = ConferenceForm(request.POST, instance=conference)
+	  if form.is_valid():
+	   #conference.title = form.cleaned_data['title']
+	   #pass
+	   form.save()
+	      # do something.
+	else:
+	  form = ConferenceForm(instance=conference)
+
+	
+        return render_to_response("conference/edit_conference.html", {
+         "form": form, "conf_id" : conf_id , "conference" : conference
+	},context_instance=RequestContext(request))
+      
+      
