b8bf92805360c1581bbda3065a7ec87e312eefa9
Nicolas Bazire <nicolas.bazire@gmail.com>
2012-02-22 11:09:09 +0100

    removed unecessary form

--- confucius/forms/__init__.py
+++ confucius/forms/__init__.py
@@ -1,3 +1,2 @@
 from confucius.forms.account import (AddressFormSet, EmailFormSet, UserForm, UserCreationForm)
-from confucius.forms.confadminform import (CreateAdminForm)
 from confucius.forms.editconf_forms import (EditConfForm)
