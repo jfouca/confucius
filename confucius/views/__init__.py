f6e3b347c36bb7ab7ff86f4b014e129dc7340ada
Nicolas Bazire <nicolas.bazire@gmail.com>
2012-02-22 12:26:48 +0100

    changed module name

--- confucius/views/__init__.py
+++ confucius/views/__init__.py
@@ -1,2 +1,2 @@
 from confucius.views.account import (close_account, confirm_close_account, edit_account)
-from confucius.views.confviews import (list_conference, edit_conference, home_conference, change_conference, close_conference, open_conference)
+from confucius.views.conference import (list_conference, edit_conference, home_conference, change_conference, close_conference, open_conference)
