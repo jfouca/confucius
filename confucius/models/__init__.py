6bcf66e307fb7ab3ebe62a439125bd0781cb1181
Nicolas Bazire <nicolas.bazire@gmail.com>
2012-02-22 11:08:23 +0100

    cleaning up, changed Account to User entity

--- confucius/models/__init__.py
+++ confucius/models/__init__.py
@@ -1,1 +1,2 @@
 from confucius.models.account import (Address, BaseConfucius, Email, Language)
+from confucius.models.confmodels import (Conference, Alert, Role, ConferenceAccountRole, MessageTemplate, Domain)
