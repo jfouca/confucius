0517888b9c5ae1264ebf560b9e1752051db8bf76
yanng <ygauche@gmail.com>
2012-02-22 13:53:50 +0100

    Update confucius/forms/editconf_forms.py

--- confucius/forms/editconf_forms.py
+++ confucius/forms/editconf_forms.py
@@ -6,13 +6,8 @@ from django.contrib.admin import widgets
 from django.contrib.admin.widgets import AdminDateWidget 
 
 class EditConfForm(ModelForm): 
-
-    
-
     def __init__(self, *args, **kwargs):
         super(EditConfForm, self).__init__(*args, **kwargs)
-	self.fields['domains'].help_text = ''
-	self.fields['isOpen'].label = 'Open'
         self.fields['startConfDate'].label = 'Conference starting date'
         self.fields['endConfDate'].label = 'Conference ending date'
         self.fields['startSubmitDate'].label = 'Submission starting date'
@@ -23,13 +18,4 @@ class EditConfForm(ModelForm):
 
     class Meta:
         model = Conference
-        exclude = ('title','accounts','president','help_text')
-        widgets = {
-	    'startConfDate': AdminDateWidget(),
-	    'endConfDate': AdminDateWidget(),
-	    'startSubmitDate': AdminDateWidget(),
-	    'endSubmitDate': AdminDateWidget(),
-	    'startEvaluationDate': AdminDateWidget(),
-	    'endEvaluationDate': AdminDateWidget(),	    
-	    'domains' : forms.CheckboxSelectMultiple()
-	}
+        exclude = ('title','accounts','president','help_text', 'isOpen', 'domains')
