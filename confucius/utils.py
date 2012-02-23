bfd4be71acaa65d44b764a69b2f73453d5e3bb0f
Nicolas Bazire <nicolas.bazire@gmail.com>
2012-02-23 10:36:13 +0100

    further merging

--- confucius/utils.py
+++
@@ -1,9 +0,0 @@
-from hashlib import sha1
-from base64 import b64encode
-
-
-def email_to_username(email):
-    """
-    Guaranteed to produce a 27 characters long string.
-    """
-    return b64encode(sha1(email).digest())
