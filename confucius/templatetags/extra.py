1c2925de5668538067983a0682bec54fcd3c94e7
Yann Gauche <ygauche@gmail.com>
2012-02-23 17:10:21 +0100

    new form.html

--- confucius/templatetags/extra.py
+++ confucius/templatetags/extra.py
@@ -1,7 +1,13 @@
 from django import template
 from django.core.urlresolvers import reverse
+from django.utils.safestring import mark_safe
 
 register = template.Library()
+
+@register.filter
+def no_time_left(date):
+    """for icon in subtopbar"""
+    pass
 
 
 @register.filter
@@ -9,7 +15,6 @@ def active(name, request):
     if request.path.startswith(reverse(name)):
         return 'active'
     return ''
-
 
 @register.filter
 def type(field):
@@ -23,6 +28,25 @@ def type(field):
         return 'textarea'
     if field.field.widget.__class__.__name__ is 'PasswordInput':
         return 'password'
+    if field.field.__class__.__name__ is 'DateField':
+        return 'date'
     if field.field.__class__.__name__ is 'EmailField':
         return 'email'
     return 'text'
+
+@register.filter
+def small(field):
+    return field.as_widget(attrs={'class': 'input-small'})
+
+@register.filter
+def medium(field):
+    return field.as_widget(attrs={'class': 'input-medium'})
+
+@register.filter
+def large(field):
+    return field.as_widget(attrs={'class': 'input-large'})
+
+@register.filter
+def help(field): 
+    return mark_safe('<p class="help-block">' + field.help_text + '</p>')
+    
