1b5c01746d0941ec1c8ab2d04ca8b3a7b729459f
Yann Gauche <ygauche@gmail.com>
2012-02-23 11:46:54 +0100

    extras / forms

--- confucius/templatetags/extra.py
+++ confucius/templatetags/extra.py
@@ -13,6 +13,14 @@ def active(name, request):
 
 @register.filter
 def type(field):
+    if field.field.widget.__class__.__name__ is 'Select':
+        return 'select'
+    if field.field.widget.__class__.__name__ is 'SelectMultiple':
+        return 'selectm'
+    if field.field.widget.__class__.__name__ is 'CheckboxInput':
+        return 'checkbox'
+    if field.field.widget.__class__.__name__ is 'Textarea':
+        return 'textarea'
     if field.field.widget.__class__.__name__ is 'PasswordInput':
         return 'password'
     if field.field.__class__.__name__ is 'EmailField':
