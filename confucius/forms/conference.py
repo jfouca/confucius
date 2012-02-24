7ae0998d6379de61ce5c2cde206fe1c9773888a2
Jeremy Foucault <foucault.jeremy@gmail.com>
2012-02-24 18:08:35 +0100

    v 3.0

--- confucius/forms/conference.py
+++ confucius/forms/conference.py
@@ -13,31 +13,30 @@ class AlertForm(forms.ModelForm):
         super(AlertForm, self).__init__(*args, **kwargs)
         self.fields['roles'].help_text = ''
         
-
     def clean(self):
-
-        super(AlertForm, self).clean()
-
+        cleaned_data = super(AlertForm, self).clean()
         try:
-            if self.cleaned_data['reminder'] is None and self.cleaned_data['event'] is None and self.cleaned_data['trigger_date'] is None and self.cleaned_data['action'] is None :
-               raise forms.ValidationError('You must fill at least one of the following fields : Reminder, Action, Trigger date')
-            if self.cleaned_data['reminder'] is not None and self.cleaned_data['event'] is None:
+            if cleaned_data['reminder'] is None and cleaned_data['event'] is None and cleaned_data['trigger_date'] is None and cleaned_data['action'] is None :
+                raise forms.ValidationError('You must fill at least one of the following fields : Reminder, Action, Trigger date')
+               
+            if cleaned_data['reminder'] is not None and cleaned_data['event'] is None:
                 raise forms.ValidationError('You must fill both of reminder and event fields')
-            if self.cleaned_data['event'] is not None and self.cleaned_data['reminder'] is None:
+                
+            if cleaned_data['event'] is not None and cleaned_data['reminder'] is None:
                 raise forms.ValidationError('You must fill both of reminder and event fields')
-            if self.cleaned_data['action'] is not None :
-                if self.cleaned_data['reminder'] is not None or self.cleaned_data['event'] is not None or self.cleaned_data['trigger_date'] is not None:
-                    raise forms.ValidationError('One field only must be selected between Reminder, Trigger date and Action')       
-            if self.cleaned_data['trigger_date'] is not None :
-                if self.cleaned_data['reminder'] is not None or self.cleaned_data['event'] is not None or self.cleaned_data['action'] is not None:
-                    raise forms.ValidationError('One field only must be selected between Reminder, Trigger date and Action')   
-            if self.cleaned_data['event'] is not None and self.cleaned_data['reminder'] is not None:
-                if self.cleaned_data['action'] is not None or self.cleaned_data['trigger_date'] is not None :
-                    raise forms.ValidationError('One field only must be selected between Reminder, Trigger date and Action')          
-        except KeyError:
+                
+            if cleaned_data['action'] is not None and (cleaned_data['reminder'] is not None or cleaned_data['event'] is not None or cleaned_data['trigger_date'] is not None) :
+                raise forms.ValidationError('One field only must be selected between Reminder, Trigger date and Action') 
+                          
+            if cleaned_data['trigger_date'] is not None and (cleaned_data['reminder'] is not None or cleaned_data['event'] is not None or cleaned_data['action'] is not None) :
+                raise forms.ValidationError('One field only must be selected between Reminder, Trigger date and Action')  
+                     
+            if (cleaned_data['event'] is not None and cleaned_data['reminder'] is not None) and (cleaned_data['action'] is not None or cleaned_data['trigger_date'] is not None) :
+                raise forms.ValidationError('One field only must be selected between Reminder, Trigger date and Action')
+        except:
             pass
 
-
+        return cleaned_data
 
 class ConferenceForm(forms.ModelForm):
     class Meta:
