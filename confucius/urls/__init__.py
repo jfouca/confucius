a2e3bde3378095af04d9a9970e9bcdf6f72dc1ff
Fernandes Lucas <lucskywalkerzero@gmail.com>
2012-02-24 09:14:43 +0100

    Fix urls .

--- confucius/urls/__init__.py
+++ confucius/urls/__init__.py
@@ -3,6 +3,7 @@ from django.conf.urls.defaults import include, patterns
 from confucius import admin
 
 urlpatterns = patterns('',
+    (r'^$', 'confucius.views.dashboard'),
     (r'^account/', include('confucius.urls.account')),
     (r'^conference/', include('confucius.urls.conference')),
     (r'^submission/', include('confucius.urls.submission')),
