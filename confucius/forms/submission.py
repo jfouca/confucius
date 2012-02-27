09a17addb6117aa8cb35959ea554794219769a9a
Nicolas Bazire <nicolas.bazire@gmail.com>
2012-02-27 09:01:10 +0100

    added paper description: fixed #13

--- confucius/forms/submission.py
+++ confucius/forms/submission.py
@@ -4,11 +4,11 @@ from confucius.models import Paper, Conference, Domain
 
 class PaperForm(forms.ModelForm):
     domains = forms.ModelMultipleChoiceField(Domain.objects.all(), required=True)
-    
+
     class Meta:
         model = Paper
-        fields = ('title', 'emails_authors', 'language', 'document')
-        
+        exclude = ('submitter', 'conference', 'submission_date', 'last_update_date')
+
     def __init__(self, *args, **kwargs):
         pk_conference = kwargs.pop('pk_conference')
         super(PaperForm, self).__init__(*args, **kwargs)
@@ -16,18 +16,17 @@ class PaperForm(forms.ModelForm):
         self.fields["domains"].queryset = Conference.objects.get(pk=pk_conference).domains
         paper = kwargs.pop('instance', None)
         if paper is not None:
-            self.fields["domains"].initial = paper.domains.all()    
-        
-        
+            self.fields["domains"].initial = paper.domains.all()
+
     def save(self, **kwargs):
         paper = super(PaperForm, self).save(commit=False)
-        
+
         # Update only one time these fields (during the creation)
         if paper.pk is None:
             paper.submitter = kwargs.pop('user')
             paper.conference = Conference.objects.get(pk=kwargs.pop('pk_conference'))
-            paper.save() # In order to get a primary key, for m2m relations (domains)
-        
+            paper.save()  # In order to get a primary key, for m2m relations (domains)
+
         paper.domains = self.cleaned_data['domains']
         paper.save()
 
