c227d55a9ad0ffe5526b62a426350dd17d9efe0f
Yann Gauche <ygauche@gmail.com>
2012-03-06 15:53:39 +0100

    updated lists

--- confucius/static/confucius.js
+++ confucius/static/confucius.js
@@ -1,13 +1,23 @@
-$(function() {
-    $('.datepicker').datepicker({
-        dateFormat: 'yy-mm-dd',
-    });
-    $('.toggle_modal').modal({
-        show: 0,
-    });
-    $('.toggle_conference').click(function() {
-        $(this).next().modal('show');
-        return false;
-    });
-    $('.tooltip-show').tooltip();
-});
+$(document).ready(
+    $(function() {
+        $('.datepicker').datepicker({
+            dateFormat: 'yy-mm-dd',
+        });
+        $('.toggle_modal').modal({
+            show: 0,
+        });
+        $('.toggle_conference').click(function() {
+            $(this).next().modal('show');
+            return false;
+        });
+        $('.tooltip-show').tooltip();
+        $('.row-actions').mouseenter(function() {
+                $(this).find('.data-actions').toggleClass('v-hidden');
+            }
+        ),
+        $('.row-actions').mouseleave(function() {
+                $(this).find('.data-actions').toggleClass('v-hidden');
+            }
+        );
+    })
+);
