0e3d8aa95490fb619cc6091e5a5e6b96e4a9d956
Fernandes Lucas <lucskywalkerzero@gmail.com>
2012-03-07 17:33:04 +0100

    fix

--- confucius/forms/review.py
+++ confucius/forms/review.py
@@ -8,8 +8,6 @@ class ProblemForm(forms.Form):
 
 
 class ReviewForm(forms.ModelForm):
-    #evaluation_choice = (('1', 'Strong reject'), ('2', 'Reject'), ('3', 'Weak reject'), ('4', 'Bordeline paper'), ('5', 'Weak accept'), ('6', 'Accept'), ('7', 'Strong accept'))
-    #overall_evaluation = forms.TypedChoiceField(coerce=int, choices=evaluation_choice, widget=forms.RadioSelect)
     confidence_choice = (('1', 'Null'), ('2', 'Low'), ('3', 'Medium'), ('4', 'High'), ('5', 'Expert'))
     reviewer_confidence = forms.TypedChoiceField(coerce=int, choices=confidence_choice, widget=forms.RadioSelect)
 
@@ -21,15 +19,12 @@ class ReviewForm(forms.ModelForm):
         super(ReviewForm, self).__init__(*args, **kwargs)
         review = kwargs.pop('instance', None)
         if review is not None:
-            #self.fields["overall_evaluation"].initial = review.overall_evaluation
             self.fields["reviewer_confidence"].initial = review.reviewer_confidence
         else:
-            #self.fields["overall_evaluation"].initial = '4'
-            self.fields["reviewer_confidence"].initial = '3'
+            self.fields["reviewer_confidence"].initial = '0'
 
     def save(self, **kwargs):
         review = super(ReviewForm, self).save(commit=False)
-        #review.overall_evaluation = self.cleaned_data['overall_evaluation']
         review.reviewer_confidence = self.cleaned_data['reviewer_confidence']
         evaluation = kwargs.pop('overall_evaluation', None)
         review.overall_evaluation = evaluation
